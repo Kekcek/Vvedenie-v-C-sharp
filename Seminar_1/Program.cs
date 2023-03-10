43a573b5483449f995756b54b3eef05b79e5572b
Киселев Иван <kiseleff2010@yandex.ru>
2023-03-10 15:51:07 +0300

    Добавлена информация про типы данных

---
+++ Seminar_1/Program.cs
@@ -0,0 +1,7 @@
+﻿int intgerNum = 32;
+double doubleNum = 2.31234;
+
+string word = "sfsdf";
+char symbol = 'f'; // Здесь можно только символы и только один
+
+bool check = true; // Логический тип данных, используется для проверки условий
