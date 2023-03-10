9c01bfb755f246462a5cc00e7a3d407e68ffecd2
Киселев Иван <kiseleff2010@yandex.ru>
2023-03-10 16:00:37 +0300

    Добавлена ПЕРВАЯ задача

--- Seminar_1/Program.cs
+++ Seminar_1/Program.cs
@@ -6,10 +6,28 @@ string word = "sfsdf";
 char symbol = 'f'; // Здесь можно только символы и только один
 
 bool check = true; // Логический тип данных, используется для проверки условий
-*/
+
 
 int num = 5;
 Console.WriteLine($"My number is {num}"); // $ и {} используется для того, чтобы можно было выводить значение переменных без конкатенации
 
 Console.Write("Input a number: ");
 num = Convert.ToInt32(Console.ReadLine());
+*/
+
+// Seminar 1 
+// Task 1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+
+Console.Write("Input a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int quad2 = num2 * num2;
+
+if (num1 == quad2)
+    Console.WriteLine("Yes!");
+else
+    Console.WriteLine("No!");
+
