bf30f1876024b84a0ddb0586ff226b4ed12468f3
Киселев Иван <kiseleff2010@yandex.ru>
2023-03-10 15:55:27 +0300

    Добавлена информация про вывод и ввод информации

--- Seminar_1/Program.cs
+++ Seminar_1/Program.cs
@@ -1,7 +1,15 @@
-﻿int intgerNum = 32;
+﻿/*
+int intgerNum = 32;
 double doubleNum = 2.31234;
 
 string word = "sfsdf";
 char symbol = 'f'; // Здесь можно только символы и только один
 
 bool check = true; // Логический тип данных, используется для проверки условий
+*/
+
+int num = 5;
+Console.WriteLine($"My number is {num}"); // $ и {} используется для того, чтобы можно было выводить значение переменных без конкатенации
+
+Console.Write("Input a number: ");
+num = Convert.ToInt32(Console.ReadLine());
