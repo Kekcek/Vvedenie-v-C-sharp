a037bc1607b98e11bf79e805c670e7f07ca5a3cf
Киселев Иван <kiseleff2010@yandex.ru>
2023-04-03 14:53:30 +0300

    Добавлено ДЗ по 7 семинару

---
+++ Homework_7/Program.cs
@@ -0,0 +1,168 @@
+﻿// HOMEWORK 7
+
+// Task 1. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+/*
+double[,] CreateRandomArray()
+{
+    Console.WriteLine("Введите количество строк массива");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите количество столбцов массива");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите минимальное значение элементов массива");
+    int minValue = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите максимальное значение элементов массива");
+    int maxValue = Convert.ToInt32(Console.ReadLine());
+
+    double[,] array = new double[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = Math.Round(new Random().Next(minValue, maxValue + 1) + new Random().NextDouble(), 2);
+        }
+        
+    }
+    return array;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] array = CreateRandomArray();
+PrintArray(array);
+*/
+
+// Task 2. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+//  и возвращает значение этого элемента или же указание, что такого элемента нет.
+/*
+int[,] CreateRandomArray()
+{
+    Console.WriteLine("Введите количество строк массива");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите количество столбцов массива");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите минимальное значение элементов массива");
+    int minValue = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите максимальное значение элементов массива");
+    int maxValue = Convert.ToInt32(Console.ReadLine());
+
+    int[,] array = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+        
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void CheckNumberInArray(int[,] array, int positionX, int positionY)
+{
+    if (positionX < array.GetLength(0) && positionY < array.GetLength(1))
+        Console.WriteLine(array[positionX,positionY]);
+    else
+        Console.WriteLine("Числа с такими индексами в массиве нет");
+}
+
+int[,] array = CreateRandomArray();
+Console.WriteLine("Введите первый индекс элемента");
+int positionX = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второй индекс элемента");
+int positionY = Convert.ToInt32(Console.ReadLine());
+PrintArray(array);
+CheckNumberInArray(array, positionX, positionY);
+*/
+
+// Task 3. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+/*
+int[,] CreateRandomArray()
+{
+    Console.WriteLine("Введите количество строк массива");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите количество столбцов массива");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите минимальное значение элементов массива");
+    int minValue = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите максимальное значение элементов массива");
+    int maxValue = Convert.ToInt32(Console.ReadLine());
+
+    int[,] array = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+        
+    }
+    return array;
+}
+
+void PrintArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[] SearchArithmeticMean(int[,] array)
+{
+    double[] arithMean = new double[array.GetLength(1)];
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double temp = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            temp = temp + array[i, j];
+        }
+        arithMean[j] = temp / array.GetLength(0);
+        temp = 0;
+    }
+    return arithMean;
+}
+
+void PrintArray1D(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+int[,] array = CreateRandomArray();
+PrintArray2D(array);
+double[] arithMean = SearchArithmeticMean(array);
+Console.WriteLine();
+PrintArray1D(arithMean);
+*/
